ec33e72f9d7cd10bf6bf125c6c32c37149d4adac
Jun <wnsgur4092@gmail.com>
2022-09-05 15:27:10 +1000

    Initial Commit

---
+++ Fructus-SwiftUI/Fructus_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fructus_SwiftUIApp.swift
+//  Fructus-SwiftUI
+//
+//  Created by JunHyuk Lim on 5/9/2022.
+//
+
+import SwiftUI
+
+@main
+struct Fructus_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
